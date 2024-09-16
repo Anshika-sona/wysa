78866c6c6975f562be3c59f50e164c6cc033a1ba
Anshika <97749793+Anshika-sona@users.noreply.github.com>
2024-09-16 09:11:08 +0530

    Update sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-// Replace with your own PostgreSQL database credentials
+
 const sequelize = new Sequelize('sleep_assessment_db', 'postgres', 'password', {
   host: 'localhost',
   dialect: 'postgres'
